75ca7a8139a72e67ceb971a36a10078394c5e528
A17-MD <adamreina18@gmail.com>
2022-09-28 14:19:16 +0700

    Update hwkal.js

--- hwkal.js
+++ hwkal.js
@@ -14,24 +14,24 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
 global.youtube = 'https://youtube.com/c/HwModsWa857'
-global.ig = 'https://www.instagram.com/hikal_857/?hl=id'
+global.ig = 'https://www.instagram.com/reina.adam47/?hl=id'
 global.mygc = 'wa.me/6285714170944'
 global.myweb = 'https://eclass.iainsalatiga.ac.id/app/upload/users/1/10892/my_files/XhiroMhonshine.html'
 global.linkgrupss = "https://chat.whatsapp.com/Jh7zaCSnPCBC4gdYOEVm0K"
 global.linkpro = 'mailto:support@whatsapp.com?subject=Deactivate%20My%20Account%20Number&body=Please%20Deactivate%20The%20My%20Account%20Number%20%2B62XXXXXXXX%20Immediately%20Because%20The%20Number%20Has%20Been%20Lost'
-global.email = 'denyp857@gmail.com'
-global.region = 'indonesia'
+global.email = 'a17ftmegawati@gmail.com'
+global.region = 'SUMEDANG PRIDE'
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'HW MODS WA' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'HW MODS WA' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Ir. adam' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'A17-MD' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Ir. adam' //ubah jadi nama mu, note tanda ' gausah di hapus!
 //=================================================//
 // Other
-global.owner = ['6281214281312']
-global.premium = ['6281214281312']
+global.owner = ['6285723745866']
+global.premium = ['6285723745866']
 global.packname = 'HW MODS WA'
-global.ownerr = ['HW MODS WA']
-global.author = 'HW MODS WA'
+global.ownerr = ['Ir. adam']
+global.author = '©A17-MD'
 global.sessionName = './sessionye/haikal'
 global.prefa = ['','!','.',',','🐤','🗿']
 global.sp = '⭔'
